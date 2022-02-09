9951d4a16c93c2300d59f0a2b1e7e5e70e7753b9
Gy <334010037@qq.com>
2022-02-09 17:13:29 +0800

    spring demo

---
+++ boot-05-web-01/src/test/java/com/hnu/boot05web01/Boot05Web01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnu.boot05web01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot05Web01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
